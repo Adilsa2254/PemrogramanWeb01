0cc8fde1874f2c76d9522e78143893ff8c2ef1d9
Adilsa2254 <al.saptr4@gmail.com>
2026-05-22 15:40:14 +0700

    coba coba js

---
+++ page_pengerjaan_tugas/Latihan_js2/Js.js
@@ -0,0 +1,6 @@
+function btn_click_image(){
+    document.getElementById('imgclick').src='resource/image.png';
+}
+function btn_click_image2(){
+    document.getElementById('imgclick').src='resource/image2.png';
+}
